99d4ffa05d3c96be40155092f59d7ac66e835a3c
papb <papb1996@gmail.com>
2018-02-21 20:26:48 -0300

    Handle missing stack trace & bug fix

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 
 function jsonifyError(error) {
     var superclasses = [];
-    var temp = Object.getPrototypeOf(Object.getPrototypeOf(error));
+    var temp = Object.getPrototypeOf(error);
+    if (temp !== null) temp = Object.getPrototypeOf(temp);
     while (temp !== null) {
         superclasses.push(temp.constructor.name);
         temp = Object.getPrototypeOf(temp);
     }
     var wrappedError = {};
-    wrappedError.name = error.name;
-    wrappedError.message = error.message;
+    wrappedError.name = error.name || "<no name available>";
+    wrappedError.message = error.message || "<no message available>";
     wrappedError.superclasses = superclasses;
     wrappedError.enumerableFields = {};
     for (let x in error) {
         wrappedError.enumerableFields[x] = error[x];
     }
-    wrappedError.stack = error.stack.split('\n').map(x => x.replace(/^\s+/, ""));
+    if (typeof error.stack === "string") {
+        wrappedError.stack = error.stack.split('\n').map(x => x.replace(/^\s+/, ""));
+    } else {
+        wrappedError.stack = error.stack || "<no stack trace available>";
+    }
     return wrappedError;
 }
 
