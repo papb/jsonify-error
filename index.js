1005f089c5a2b8e9737827d1b612b182f8b10734
papb <papb1996@gmail.com>
2018-10-27 23:31:16 -0200

    feat: Add overrideErrorMethods()

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const jsonifyError = require("./lib/jsonify-error");
 const overrideConsole = require("./lib/override-console");
+const overrideErrorMethods = require("./lib/override-error-methods");
 const log = require("./lib/log");
 
 module.exports = jsonifyError;
 module.exports.overrideConsole = overrideConsole;
+module.exports.overrideErrorMethods = overrideErrorMethods;
 module.exports.log = log;
--- test/node/test.js
+++ test/node/test.js
@@ -22,4 +22,23 @@ describe('jsonifyError', function() {
         expect(jsonified.stack[0]).to.equal("Error: Some message");
     });
 
+    it('should override error methods correctly', function() {
+        const ErrorSubclass = class ErrorSubclass extends Error {};
+        const e = new ErrorSubclass("Some message");
+        jsonifyError.overrideErrorMethods();
+
+        const jsonified = e.toJSON();
+        expect(jsonified).to.deep.include({
+            name: "Error",
+            className: "ErrorSubclass",
+            message: "Some message",
+            superclasses: ["Error", "Object"],
+            enumerableFields: {}
+        });
+        expect(jsonified.stack).to.be.an("array");
+        expect(jsonified.stack[0]).to.equal("Error: Some message");
+        
+        expect(e.toString(7)).to.match(/^ErrorSubclass: Some message {\n {7}"name": "Error"/);
+    });
+
 });
