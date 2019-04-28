9f151635ea148343729ffd9efca40c6a77c2f1be
papb <papb1996@gmail.com>
2019-04-28 11:52:52 -0300

    Use LF instead of CRLF

--- helpers/ensure-lf.js
+++ helpers/ensure-lf.js
@@ -1,10 +1,10 @@
 "use strict";
 
-// This script is used to ensure CRLF in all dist files.
+// This script is used to ensure LF in all dist files.
 
 const jetpack = require("fs-jetpack");
 
 jetpack.list("dist").forEach(file => {
-    const content = jetpack.read("dist/" + file).replace(/\r?\n/g, "\r\n");
+    const content = jetpack.read("dist/" + file).replace(/\r?\n/g, "\n");
     jetpack.write("dist/" + file, content);
 });
